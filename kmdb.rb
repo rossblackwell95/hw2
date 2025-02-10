5912646d28ec8f3d59f1d672be1a4fb24cd3326d
Ross Blackwell <ross.blackwell@kellogg.northwestern.edu>
2025-02-10 20:26:21 +0000

    Models and Tables Generated

--- kmdb.rb
+++ kmdb.rb
@@ -75,14 +75,22 @@
 
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
-# TODO!
+Studio.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+Role.destroy_all
 
 # Generate models and tables, according to the domain model.
-# TODO!
+# rails generate model Studio DONE
+# rails generate model Movie DONE
+# rails generate model Actor DONE
+# rails generate model Role DONE
+# rails db:migrate DONE
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
-# TODO!
+
+
 
 # Prints a header for the movies output
 puts "Movies"
